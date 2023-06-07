189749f0bc2260e363588624a8fed8570febbaa0
enzoenrico <e.enrico2005@gmail.com>
2023-06-07 15:20:29 -0300

    links arrumados

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,21 +1,21 @@
 export default [
    {
-      title: 'Portfolio',
-      imgUrl: 'src/assets/important.jpg',
+      title: 'Portfolio 2.0',
+      imgUrl: 'https://media.licdn.com/dms/image/D4D2DAQFUWQBhgLLWng/profile-treasury-image-shrink_800_800/0/1686161850088?e=1686769200&v=beta&t=-UBLOzqgOuzd-kAkW-ij5OfdhNZkP-6KMQsc-HYfazA',
       stack: ['ReactJS', 'TailWind Css', 'Figma', 'Git'],
-      link: 'https://fkcodes.com',
+      link: 'https://github.com/enzoenrico/portifoliov2',
    },
    {
       title: 'Website React + Tailwind',
-      imgUrl: 'src/assets/important.jpg',
+      imgUrl: 'https://media.licdn.com/dms/image/D4D2DAQGDzoPo2nK1nw/profile-treasury-image-shrink_800_800/0/1686161532831?e=1686769200&v=beta&t=8kKbwaYLPll-yCIxe4tWiipsm1AUTfpZNG-MlqCCM1k',
       stack: ['ReactJS', 'TailWind Css'],
-      link: 'https://fkcodes.com',
+      link: 'https://github.com/enzoenrico/portifolioReactTailwind',
    },
    {
       title: 'uMirror',
-      imgUrl: 'src/assets/important.jpg',
+      imgUrl: 'https://media.licdn.com/dms/image/C4D2DAQHm1LfJ6drYgg/profile-treasury-image-shrink_800_800/0/1674443744985?e=1686769200&v=beta&t=40VSdS4Bmg9HapKo-NZYyNsm2D2AyJqzPAvnDyg03e4',
       stack: ['Python', 'NodeJs', 'MongoDb'],
-      link: 'https://thehackershideout.com',
+      link: 'https://github.com/enzoenrico/uMirrorSoftware',
    }
    
 ]

59b83829f69a298f18d11918b91144fbc15693cc
enzoenrico <e.enrico2005@gmail.com>
2023-06-14 21:50:54 -0300

    projeto alterados

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,19 +1,19 @@
 export default [
    {
-      title: 'Portfolio 2.0',
-      imgUrl: 'https://media.licdn.com/dms/image/D4D2DAQFUWQBhgLLWng/profile-treasury-image-shrink_800_800/0/1686161850088?e=1686769200&v=beta&t=-UBLOzqgOuzd-kAkW-ij5OfdhNZkP-6KMQsc-HYfazA',
+      title: 'ClassMinder',
+      imgUrl: 'assets/LogoProduto.png',
       stack: ['ReactJS', 'TailWind Css', 'Figma', 'Git'],
-      link: 'https://github.com/enzoenrico/portifoliov2',
+      link: 'https://github.com/enzoenrico/classCalendar',
    },
    {
-      title: 'Website React + Tailwind',
-      imgUrl: 'https://media.licdn.com/dms/image/D4D2DAQGDzoPo2nK1nw/profile-treasury-image-shrink_800_800/0/1686161532831?e=1686769200&v=beta&t=8kKbwaYLPll-yCIxe4tWiipsm1AUTfpZNG-MlqCCM1k',
+      title: 'Portifolio React + Tailwind',
+      imgUrl: 'assets/portifImg.png',
       stack: ['ReactJS', 'TailWind Css'],
       link: 'https://github.com/enzoenrico/portifolioReactTailwind',
    },
    {
       title: 'uMirror',
-      imgUrl: 'https://media.licdn.com/dms/image/C4D2DAQHm1LfJ6drYgg/profile-treasury-image-shrink_800_800/0/1674443744985?e=1686769200&v=beta&t=40VSdS4Bmg9HapKo-NZYyNsm2D2AyJqzPAvnDyg03e4',
+      imgUrl: 'assets/umiro.png',
       stack: ['Python', 'NodeJs', 'MongoDb'],
       link: 'https://github.com/enzoenrico/uMirrorSoftware',
    }

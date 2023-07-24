50ceff41d3ad35b46407ce4777aa3dfc0c995989
enzoenrico <e.enrico2005@gmail.com>
2023-07-24 20:50:45 -0300

    QOL

--- src/components/Console.jsx
+++ src/components/Console.jsx
@@ -7,9 +7,9 @@ function Console() {
     
     <div className="p-3">
       <div className="dark:bg-slate-300 w-100 box-border h-7 bg-slate-200 mx-0 my-auto rounded-t-md">
-        <div className="h-2.5 w-2.5 rounded-full relative top-1 left-2 bg-red-600 inline-block"></div>
-        <div className="h-2.5 w-2.5 rounded-full relative top-1 left-3 bg-yellow-300 inline-block"></div>
-        <div className="h-2.5 w-2.5 rounded-full relative top-1 left-4 bg-green-600 inline-block"></div>
+        <div className="h-2.5 w-2.5 rounded-full relative top-1 left-2 bg-red-600 inline-block hover:scale-110"></div>
+        <div className="h-2.5 w-2.5 rounded-full relative top-1 left-3 bg-yellow-300 inline-block hover:scale-110"></div>
+        <div className="h-2.5 w-2.5 rounded-full relative top-1 left-4 bg-green-600 inline-block hover:scale-110"></div>
       </div>
       <div className="dark:bg-slate-900 bg-slate-300 box-border w-100 mx-0 my-auto p-5 rounded-b-md">
         <Intro />
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,25 +1,34 @@
 import React from "react";
 
-
 function Intro() {
   return (
     <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
       {/* Add hover pra highlight só do @dev */}
-      <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold hover:text-pink-500 transition-colors duration-300">
-        {"$~"}enzo@dev
+      <h1
+        id="scrambled"
+        className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold hover:text-pink-500 transition-colors duration-300 flex justify-center items-center"
+      >
+        <h1 className="text-6xl animate-bounce">👋</h1> {""}enzo@dev
       </h1>
 
       <p className="text-base md:text-x1 mb-3 font-medium">
         Engenheiro de Software & Desenvolvedor Web
       </p>
-      <p className="text-small mw-w-1 mb-6 font-bold dark:bg-slate-200 bg-slate-800 dark:text-stone-800 text-white rounded-md p-4 md:m-2 sm: m-2">
-        ~👾 Sou um desenvolvedor full-stack, estudante e entusiasta de
-        tudo que é tecnologia! Atualmente cursando Engenharia de Software {" "}
-        <a href="https://www.pucpr.br/cursos-graduacao/engenharia-de-software/" className="dark:text-blue-600 text-pink-500 hover:text-blue-900 hover:underline decoration-2 transition-colors duration-300 hover:cursor-pointer">
-          @PUCPR {" "}
+      <p className="text-small mw-w-1 mb-6 font-bold dark:bg-slate-200 bg-slate-800 dark:text-stone-800 text-white rounded-md p-4 md:m-2 sm: m-2 ">
+        <text className="animate-pulse">~👾</text> Sou um desenvolvedor
+        full-stack, estudante e entusiasta de tudo que é tecnologia! Atualmente
+        cursando Engenharia de Software{" "}
+        <a
+          href="https://www.pucpr.br/cursos-graduacao/engenharia-de-software/"
+          className="dark:text-blue-600 text-pink-500 hover:text-blue-900 hover:underline decoration-2 transition-colors duration-300 hover:cursor-pointer"
+        >
+          @PUCPR{" "}
         </a>
-         e estagiando{" "}
-        <a href="https://www.akersolutions.com" className="dark:text-blue-600 text-pink-500 hover:text-blue-900 hover:underline decoration-2 transition-colors duration-300 hover:cursor-pointer">
+        e estagiando{" "}
+        <a
+          href="https://www.akersolutions.com"
+          className="dark:text-blue-600 text-pink-500 hover:text-blue-900 hover:underline decoration-2 transition-colors duration-300 hover:cursor-pointer"
+        >
           @AkerSolutions!
         </a>
         <p className="animate-pulse">|</p>

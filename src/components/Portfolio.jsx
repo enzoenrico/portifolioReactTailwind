e5de497ca82a4bce95ac757b5729a22faf8d0b75
Enzo Enrico <e.enrico2005@gmail.com>
2024-06-24 12:08:09 -0300

    ......

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -7,13 +7,14 @@ import Title from "./Title";
 import ArticleItem from "./ArticleItem";
 
 function Portfolio() {
-  const [data, setData] = useState(new Set())
+  const [data, setData] = useState([])
   const [isLoading, setIsloading] = useState(true)
 
   useEffect(() => {
     fetch('https://dev.to/api/articles?username=enzoenrico').then(response => {
       return response.json()
     }).then(data => {
+      console.log(data)
       setData(data)
       setIsloading(false)
     }
@@ -31,26 +32,26 @@ function Portfolio() {
       <div className="flex flex-col md:flex-row items-center justify-center p-5 ">
         <div className={`md:flex md:flex-row max-w-full overflow-x-auto dark:dark-scrollbar light-scrollbar md:gap-4 md:scroll-smooth md:p-3 flex flex-col gap-4 p-2`} >
           {data.map((item) => (
-          <ArticleItem
-            key={item.id}
-            title={item.title}
-            stack={[...item.tag_list]}
-            imageUrl={item.cover_image}
-            url={item.url}
-          />
-        ))}
-        {portfolio.map((project) => (
-          <PortfolioItem
-            imgUrl={project.imgUrl}
-            title={project.title}
-            stack={project.stack}
-            link={project.link}
+            <ArticleItem
+              key={item.id}
+              title={item.title}
+              stack={[...item.tag_list]}
+              imageUrl={item.cover_image}
+              url={item.url}
+            />
+          ))}
+          {portfolio.map((project) => (
+            <PortfolioItem
+              imgUrl={project.imgUrl}
+              title={project.title}
+              stack={project.stack}
+              link={project.link}
 
-          />
-        ))}
+            />
+          ))}
 
-      </div>
-    </div >
+        </div>
+      </div >
     </>
   );
 }

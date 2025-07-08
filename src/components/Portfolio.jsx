aa9dc43d196618fabdef4ad53acae04e4c92f48d
enzo <e.enrico2005@gmail.com>
2025-07-08 18:20:50 -0300

    feat: updated experience

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,66 +1,63 @@
 /* eslint-disable react/jsx-key */
-import { useState, useEffect } from "react";
-import portfolio from "../data/portfolio";
+import { useState, useEffect } from 'react'
+import portfolio from '../data/portfolio'
 // import blogPosts from "../data/blogPosts";
-import PortfolioItem from "./PortfolioItem";
-import Title from "./Title";
-import ArticleItem from "./ArticleItem";
-import { FaSpinner } from "react-icons/fa6";
-import { FaTerminal, FaTruckLoading } from "react-icons/fa";
+import PortfolioItem from './PortfolioItem'
+import Title from './Title'
+import ArticleItem from './ArticleItem'
+import { FaSpinner } from 'react-icons/fa6'
+import { FaTerminal, FaTruckLoading } from 'react-icons/fa'
 
-function Portfolio() {
-    const [data, setData] = useState([])
-    const [isLoading, setIsloading] = useState(true)
+function Portfolio () {
+  const [data, setData] = useState([])
+  const [isLoading, setIsloading] = useState(true)
 
-    useEffect(() => {
-        fetch('https://dev.to/api/articles?username=enzoenrico').then(response => {
-            return response.json()
-        }).then(data => {
-            setData(data, ...data)
-            setIsloading(false)
-        }
-        )
-    }, [])
+  useEffect(() => {
+    fetch('https://dev.to/api/articles?username=enzoenrico')
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        setData(data, ...data)
+        setIsloading(false)
+      })
+  }, [])
 
-    return (
-        <>
-            <Title>Projects & Articles</Title>
-            {
-                isLoading ? (
-
-                    <div className="flex flex-col md:flex-row items-center justify-center p-5 animate-spin text-4xl">
-                        <FaSpinner />
-                    </div>
-                ) : (
-
-                    <div className="flex flex-col md:flex-row items-center justify-center p-5 ">
-                        <div className={`md:flex md:flex-row max-w-full overflow-x-auto dark:dark-scrollbar light-scrollbar md:gap-4 md:scroll-smooth md:p-3 flex flex-col gap-4 p-2`} >
-                            {portfolio.map((project) => (
-                                <PortfolioItem
-                                    imgUrl={project.imgUrl}
-                                    title={project.title}
-                                    // stack={project.stack.map((item) => item.toLowerCase())}
-                                    stack={project.stack}
-                                    link={project.link}
-
-                                />
-                            ))}
-                            {data.map((item) => (
-                                <ArticleItem
-                                    key={item.id}
-                                    title={item.title}
-                                    stack={[...item?.tags.split(",")]}
-                                    imageUrl={item.cover_image}
-                                    url={item.url}
-                                />
-                            ))}
-
-                        </div>
-                    </div >
-                )
-            }
-        </>
-    );
+  return (
+    <>
+      <Title>Projects & Articles</Title>
+      {isLoading ? (
+        <div className='flex flex-col md:flex-row items-center justify-center p-5 animate-spin text-4xl'>
+          <FaSpinner />
+        </div>
+      ) : (
+        <div className='flex flex-col md:flex-row items-center justify-center p-5 '>
+          <div
+            className={`md:flex md:flex-row max-w-full overflow-x-auto dark:dark-scrollbar light-scrollbar md:gap-4 md:scroll-smooth md:p-3 flex flex-col gap-4 p-2`}
+          >
+            {portfolio.map(project => (
+              <PortfolioItem
+                imgUrl={project.imgUrl}
+                title={project.title}
+                // stack={project.stack.map((item) => item.toLowerCase())}
+                stack={project.stack}
+                link={project.link}
+              />
+            ))}
+            {data.map(item => (
+              <ArticleItem
+                key={item.id}
+                title={item.title}
+                stack={[...item.tags.split(',')]}
+                imageUrl={item.cover_image}
+                url={item.url}
+              />
+            ))}
+          </div>
+        </div>
+      )}
+    </>
+  )
 }
 
-export default Portfolio;
+export default Portfolio
--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,25 +1,19 @@
 export default [
     {
-        title: 'Writing a Obsidian integration',
-        imgUrl: "https://miro.medium.com/v2/resize:fit:720/format:webp/1*ISo4rsHAqPN2_Aq33f_y2w.png",
-        stack: ['Automation', 'Organization', 'Leetcode', 'Obsidian'],
-        link: 'https://medium.com/@e.enrico2005/getting-my-daily-leetcode-question-in-obsidian-0d85739a8cad',
+        title: "DeapLeap",
+        imgUrl: 'assets/deapleap.png',
+        stack: ['Swift', 'Machine Learning', 'Mobile', 'IOS'],
+        link: ""
     },
     {
-        title: 'your digital self',
-        imgUrl: '/public/assets/demo.gif',
-        stack: ['AI', ';)'],
-        link: 'https://github.com/enzoenrico/ai_rg'
-    },
-    {
-        title: 'Intro 2 AI: Developing smart agents',
-        imgUrl: "https://media.licdn.com/dms/image/v2/D4D22AQGVMeLr3-jTpA/feedshare-shrink_1280/feedshare-shrink_1280/0/1727351690303?e=1733356800&v=beta&t=4qURjFpws0ZKrv7c4dfoek669LzfFQrfyh0XaBwFx-c",
+        title: 'Intro to AI: Developing smart agents',
+        imgUrl: "assets/poliweek_300.jpg",
         stack: ['AI', 'Langchain', 'OpenAI', 'Agents'],
         link: 'https://www.linkedin.com/feed/update/urn:li:activity:7245038113381642241/',
     },
     {
-        title: 'Intro 2 AI: Creating a RAG system',
-        imgUrl: 'https://media.licdn.com/dms/image/v2/D4D22AQEohAlWxXZdEA/feedshare-shrink_2048_1536/feedshare-shrink_2048_1536/0/1725992729877?e=1733356800&v=beta&t=YBLg0crNaj0iAAWCtw3_nOfCWnFJD71MWxwXq5-tXiM',
+        title: 'Intro to AI: Creating a RAG system',
+        imgUrl: 'assets/poliweek_rag.jpg',
         stack: ['AI', 'Langchain', 'Python', 'Intro'],
         link: 'https://www.linkedin.com/posts/enzo-enrico-a71771242_ia-inteligaeanciaartificial-tecnologia-activity-7239338217022341120-ybuy?utm_source=share&utm_medium=member_desktop',
     },
@@ -28,6 +22,12 @@ export default [
         imgUrl: 'assets/teaching.jpg',
         stack: ['Algorithms', 'Data-Stuctures', 'Python'],
         link: 'https://www.linkedin.com/feed/update/urn:li:activity:7115381992023601153/',
+    },
+    {
+        title: 'Writing a Obsidian integration',
+        imgUrl: "https://miro.medium.com/v2/resize:fit:720/format:webp/1*ISo4rsHAqPN2_Aq33f_y2w.png",
+        stack: ['Automation', 'Organization', 'Leetcode', 'Obsidian'],
+        link: 'https://medium.com/@e.enrico2005/getting-my-daily-leetcode-question-in-obsidian-0d85739a8cad',
     },
     {
         title: 'uMirror',

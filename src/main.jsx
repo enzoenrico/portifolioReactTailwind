d2b21e7d89f61c37dc3d93259d96e8bfa07929ff
enzo <e.enrico2005@gmail.com>
2025-08-22 18:41:25 -0300

    feat: privacy policy and new cv

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Privacy from './components/Privacy'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/privacy' element={<Privacy />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 )
